f3da02a092c44c20b7735c435f1200097487de01
DevKaua1 <kauaribcaldeira@gmail.com>
2025-04-15 10:03:14 -0300

    Adicionando Dockerfile para deploy no Render

--- Back-end/bspmobility/src/main/java/com/bsp/bspmobility/BspmobilityApplication.java
+++ Back-end/bspmobility/src/main/java/com/bsp/bspmobility/BspmobilityApplication.java
@@ -1,13 +1,12 @@
 package com.bsp.bspmobility;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BspmobilityApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BspmobilityApplication.class, args);
-	}
-
-}
+    public static void main(String[] args) {
+        SpringApplication.run(BspmobilityApplication.class, args);
+    }
+}	
