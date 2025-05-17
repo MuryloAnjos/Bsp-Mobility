487f0d4ec033fafd4c515ec90b35b2587359feaa
DevKaua1 <kauaribcaldeira@gmail.com>
2025-05-17 17:43:09 -0300

    Atualização da API

--- Back-end/bspmobility/src/main/java/com/bsp/bspmobility/service/RotaService.java
+++ Back-end/bspmobility/src/main/java/com/bsp/bspmobility/service/RotaService.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ResponseStatusException;
